5f4b5f64f24818bd6c46622430e821bccad75a51
Henrik Braune <hb@braune-digital.com>
2017-12-24 14:40:19 +0100

    Fixed getter in form util for 2.3

--- src/Limenius/Liform/FormUtil.php
+++ src/Limenius/Liform/FormUtil.php
@@ -36,7 +36,7 @@ class FormUtil
             return;
         }
 
-        $types[] = $formType->getBlockPrefix();
+        $types[] = $formType->getName();
 
         self::typeAncestryForType($formType->getParent(), $types);
     }
